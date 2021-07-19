6821bf37515c3de526633299c44c6a269f9e60ac
Luciano Boccardi <lboccardi@itba.edu.ar>
2021-07-19 01:19:38 -0300

    Removed datasets

--- datasets/scripts/restaurants_csv_parser.py
+++
@@ -1,7 +0,0 @@
-import pandas
-
-if __name__ == '__main__':
-
-    df = pandas.read_csv('restaurants.csv')
-    df2 = df[['address','latitude','longitude','name']]
-    df2.to_csv('restaurants_modified.csv', index=False)
